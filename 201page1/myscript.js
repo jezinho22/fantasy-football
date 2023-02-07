a3a9aa82aa5ee1bb6358b33c7c79758d20d5cf92
Jeremy Johns <jez.johns@gmail.com>
2023-02-07 13:37:27 +0000

    moved to new dir

--- 201page1/myscript.js
+++
@@ -1,9 +0,0 @@
-function letMeKnow() {
-  alert("This is an alert. So now you know");
-}
-function askMe() {
-  asked = prompt("Ask me anything. Go on. Anything");
-}
-function debugMe() {
-  console.log("No bugs so far! Plus this is what you asked: ", asked);
-}
